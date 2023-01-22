1082e5e44ee74145c19a318af5ebeb9c0710f6d6
Vignesh V Pai <vigneshvpai@gmail.com>
2023-01-22 21:38:40 +0100

    Binded actions and state with settings screen

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,14 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { connect } from "react-redux";
 
-const Settings = () => (
-  <View style={styles.container}>
-    <Text>Settings</Text>
-    <StatusBar style="auto" />
-  </View>
-);
+import {
+  toggleEmailNotification,
+  toggleSMSNotification,
+  togglePushNotification,
+} from "../redux/actions";
 
-export default Settings;
+const Settings = ({ settings }) => {
+  console.log(settings);
+  return (
+    <View style={styles.container}>
+      <Text>Settings</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  settings: state.settings,
+});
+
+export default connect(mapStateToProps, {
+  toggleEmailNotification,
+  toggleSMSNotification,
+  togglePushNotification,
+})(Settings);
 
 const styles = StyleSheet.create({
   container: {
